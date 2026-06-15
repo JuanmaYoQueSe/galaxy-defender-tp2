ff4eba7d64a86efed1cc38b30d2ce246d958862e
JuanmaYoQueSe <jmcancila@gmail.com>
2026-06-15 02:18:56 -0300

    reset2

--- server.js
+++
@@ -1,8 +0,0 @@
-
-const WebSocket=require('ws');
-const wss=new WebSocket.Server({"wss://gamehubmanager.azurewebsites.net/ws"});
-let ranking=[{name:'Jugador',score:0}];
-wss.on('connection',ws=>{
- ws.send(JSON.stringify(ranking,null,2));
-});
-console.log('WS listo en puerto 8080');
